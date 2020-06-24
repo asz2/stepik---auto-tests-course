b648ced0ee429f8c876f8bf59737c3692b0b9e20
asz2 <anastasiya20071@rambler.ru>
2020-06-24 19:02:05 +0300

    captcha for robots

---
+++ lessons21_step5.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+import math
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+
+
+    x_element = browser.find_element_by_css_selector("#input_value")
+    x = x_element.text
+    y = calc(x)
+    answer = browser.find_element_by_css_selector("#answer")
+    answer.send_keys(y)
+
+    option1 = browser.find_element_by_css_selector("#robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_css_selector("#robotsRule")
+    option2.click()
+
+       # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
